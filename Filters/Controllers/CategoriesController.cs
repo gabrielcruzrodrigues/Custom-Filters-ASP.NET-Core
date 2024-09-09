3925c4f03155eb5126d33e4af99979276f1a2e6d
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-09-09 08:56:47 -0300

    Add project files.

---
+++ Filters/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Filters.CustomFilters;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        [ServiceFilter(typeof(ApiLoggingFilter))]
+        public ActionResult test()
+        {
+            return Ok("Request feita em um endpoint com custom filter");
+        }
+    }
+}
---
+++ Filters/CustomFilters/ApiLoggingFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters.CustomFilters
+{
+    public class ApiLoggingFilter : IActionFilter
+    {
+        private readonly ILogger<ApiLoggingFilter> _logger;
+        public ApiLoggingFilter(ILogger<ApiLoggingFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("aplicando ação após a execução!");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("aplicando ação durante a execução!");
+        }
+    }
+}
